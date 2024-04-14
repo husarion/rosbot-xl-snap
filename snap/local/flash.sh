ec4f531a6b5be9a0038a32f25dcfc8ce34dd9e0d
dominikn <dominik.nwk@gmail.com>
2024-04-14 16:17:11 +0000

    now it's possible to use a custom fastdds profile file

--- snap/local/flash.sh
+++ snap/local/flash.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/bash
 
-# Iterate over the snap parameters and retrieve their value.
-# If a value is set, it is forwarded to the launch file.
-
 SERIAL_PORT="$(snapctl get serial-port)"
 
 # watch the log with: "journalctl -t rosbot-xl"
 logger -t ${SNAP_NAME} "flashing with serial-port=${SERIAL_PORT}"
 
 ros2 run rosbot_xl_utils flash_firmware --port $SERIAL_PORT
+
+# Define the names of the daemons using SNAP_NAME
+udp_daemon="${SNAP_NAME}.udp-daemon"
+shm_daemon="${SNAP_NAME}.shm-daemon"
+
+# Stop the UDP daemon if it is running
+if snapctl services $udp_daemon | grep -qw active; then
+    snapctl restart $udp_daemon
+    logger -t "${SNAP_NAME}" "Restarted $udp_daemon"
+fi
+
+# Check if the SHM daemon is already running
+if snapctl services $shm_daemon | grep -qw active; then
+    snapctl restart $shm_daemon
+    logger -t "${SNAP_NAME}" "Restarted $shm_daemon"
+fi
