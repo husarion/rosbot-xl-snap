37f331a6a3375c5dfa3285b4ccbe8d2db0ecbea1
dominikn <dominik.nwk@gmail.com>
2024-05-01 21:29:04 +0000

    print the serial number the right way

--- snap/local/print-serial-number.py
+++ snap/local/print-serial-number.py
@@ -1,33 +1,12 @@
 #!/usr/bin/env python3
 
+
 import subprocess
 import json
 import hashlib
 import string
 import re
-
-def get_cpu_id_from_ros_service():
-    try:
-        # Run the ROS service call command
-        result = subprocess.run(['ros2', 'service', 'call', '/get_cpu_id', 'std_srvs/srv/Trigger'], 
-                                capture_output=True, text=True, timeout=30)
-
-        # Regular expression to extract JSON from the response
-        match = re.search(r"message='(\{.*\})'", result.stdout)
-        if match:
-            json_str = match.group(1)
-            json_data = json.loads(json_str)
-            cpu_id = json_data['cpu_id']
-            return cpu_id
-        else:
-            print("JSON response not found in the output.")
-            return None
-    except subprocess.SubprocessError as e:
-        print(f"Error during ROS service call: {e}")
-        return None
-    except json.JSONDecodeError as e:
-        print(f"JSON parsing error: {e}")
-        return None
+import sys
 
 def hex_to_num(hex_str):
     # Check if the hex string is valid
@@ -46,16 +25,25 @@ def hex_to_num(hex_str):
     # Return the hash as an ASCII string
     return hash
 
-try:
-    # Obtain CPU ID from ROS service
-    cpu_id = get_cpu_id_from_ros_service()
+def main():
+    try:
+        # Check if a CPU ID was provided as an argument
+        if len(sys.argv) != 2:
+            print("Usage: python3 script.py <cpu_id>")
+            sys.exit(1)
 
-    if cpu_id:
-        # Calculate the serial number
-        serial_number = hex_to_num(cpu_id)
-        print(f"CPU ID: 0x{cpu_id}")
-        print(f"Serial Number: {serial_number}")
-    else:
-        print("Failed to obtain CPU ID.")
-except Exception as e:
-    print(f"An error occurred: {e}")
+        # Obtain CPU ID from command line argument
+        cpu_id = sys.argv[1]
+
+        if cpu_id:
+            # Calculate the serial number
+            serial_number = hex_to_num(cpu_id)
+            print(f"CPU ID: 0x{cpu_id}")
+            print(f"Serial Number: {serial_number}")
+        else:
+            print("Failed to obtain CPU ID.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
--- snap/local/serial_number_launcher.sh
+++ snap/local/serial_number_launcher.sh
@@ -1,33 +1,11 @@
 #!/usr/bin/bash
 
-# Define a function to log and echo messages
-log_and_echo() {
-    local message="$1"
-    # Log the message with logger
-    logger -t "${SNAP_NAME}" "${SNAP_NAME}.flash: $message"
-    # Echo the message to standard error
-    echo >&2 "$message"
-}
+output="$(
+ros2 service call /get_cpu_id std_srvs/srv/Trigger
+)"
 
-SERIAL_PORT="$(snapctl get serial-port)"
+# Extract the cpu_id from the output
+cpu_id=$(echo "$output" | grep -oP '"cpu_id": "\K[^"]+')
 
-# watch the log with: "journalctl -t rosbot-xl"
-log_and_echo "flashing with serial-port=${SERIAL_PORT}"
-
-ros2 run rosbot_xl_utils flash_firmware --port $SERIAL_PORT
-
-# Define the names of the daemons using SNAP_NAME
-udp_daemon="${SNAP_NAME}.udp-daemon"
-shm_daemon="${SNAP_NAME}.shm-daemon"
-
-# Stop the UDP daemon if it is running
-if snapctl services $udp_daemon | grep -qw active; then
-    snapctl restart $udp_daemon
-    log_and_echo "Restarted $udp_daemon"
-fi
-
-# Check if the SHM daemon is already running
-if snapctl services $shm_daemon | grep -qw active; then
-    snapctl restart $shm_daemon
-    log_and_echo "Restarted $shm_daemon"
-fi
+# Print the cpu_id
+print-serial-number.py $cpu_id
