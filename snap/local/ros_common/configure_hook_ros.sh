0c9a1f57414dc8ad2d278aa387bd4636c7d49b68
dominikn <dominik.nwk@gmail.com>
2024-06-11 11:52:55 +0000

    handle also namespace in ros.env

--- snap/local/ros_common/configure_hook_ros.sh
+++ snap/local/ros_common/configure_hook_ros.sh
@@ -65,8 +65,22 @@ fi
 
 # Create the ${SNAP_COMMON}/ros.env file and export variables (for bash session running ROS2)
 ROS_ENV_FILE="${SNAP_COMMON}/ros.env"
+
+# Create the ${SNAP_COMMON}/ros.env file and export variables (for bash session running ROS2)
+ROS_SNAP_ARGS="${SNAP_COMMON}/ros_snap_args"
+
 echo "export ROS_DOMAIN_ID=${ROS_DOMAIN_ID}" > "${ROS_ENV_FILE}"
 echo "export ROS_LOCALHOST_ONLY=${ROS_LOCALHOST_ONLY}" >> "${ROS_ENV_FILE}"
+
+NAMESPACE=$(snapctl get driver.namespace)
+
+# Check if the namespace is set and not empty
+if [ -n "$NAMESPACE" ]; then
+  echo "ros-domain-id=${ROS_DOMAIN_ID} ros-localhost-only=${ROS_LOCALHOST_ONLY} transport=${TRANSPORT_SETTING} driver.namespace=${NAMESPACE}" > "${ROS_SNAP_ARGS}"
+  echo "export ROS_NAMESPACE=${NAMESPACE}" >> "${ROS_ENV_FILE}"
+else
+  echo "ros-domain-id=${ROS_DOMAIN_ID} ros-localhost-only=${ROS_LOCALHOST_ONLY} transport=${TRANSPORT_SETTING} driver.namespace!" > "${ROS_SNAP_ARGS}"
+fi
 
 if [ "$TRANSPORT_SETTING" != "builtin" ]; then
   echo "export FASTRTPS_DEFAULT_PROFILES_FILE=${SNAP_COMMON}/${TRANSPORT_SETTING}.xml" >> "${ROS_ENV_FILE}"
@@ -111,15 +125,3 @@ chmod +x "${MANAGE_SCRIPT}"
 
 echo "Created ${MANAGE_SCRIPT} and made it executable"
 
-# Create the ${SNAP_COMMON}/ros.env file and export variables (for bash session running ROS2)
-ROS_SNAP_ARGS="${SNAP_COMMON}/ros_snap_args"
-
-
-NAMESPACE=$(snapctl get driver.namespace)
-
-# Check if the namespace is set and not empty
-if [ -n "$NAMESPACE" ]; then
-  echo "ros-domain-id=${ROS_DOMAIN_ID} ros-localhost-only=${ROS_LOCALHOST_ONLY} transport=${TRANSPORT_SETTING} driver.namespace=${NAMESPACE}" > "${ROS_SNAP_ARGS}"
-else
-  echo "ros-domain-id=${ROS_DOMAIN_ID} ros-localhost-only=${ROS_LOCALHOST_ONLY} transport=${TRANSPORT_SETTING} driver.namespace!" > "${ROS_SNAP_ARGS}"
-fi
