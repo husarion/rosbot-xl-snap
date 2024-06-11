43e8cc16bfc13e79fc127eb545ce87ede6ec7c8f
dominikn <dominik.nwk@gmail.com>
2024-06-11 10:55:03 +0000

    added auto ttyUSBDB detection

--- snap/local/flash_launcher.sh
+++ snap/local/flash_launcher.sh
@@ -18,11 +18,31 @@ else
     DAEMON_WAS_RUNNING=false
 fi
 
+# Source the find_ttyUSB function
+source $SNAP/usr/bin/find_ttyUSB.sh
 
-SERIAL_PORT="$(snapctl get serial-port)"
+# Get the serial-port value using snapctl
+SERIAL_PORT=$(snapctl get serial-port)
 
-# watch the log with: "journalctl -t rosbot-xl"
-log_and_echo "flashing with serial-port=${SERIAL_PORT}"
+# Check if SERIAL_PORT is set to auto
+if [ "$SERIAL_PORT" == "auto" ]; then
+  # Find the ttyUSB* device
+  SERIAL_PORT=$(find_ttyUSB "0403" "6015")
+  if [ $? -ne 0 ]; then
+    log_and_echo "Failed to find the serial port."
+    exit 1
+  else
+    log_and_echo "Found serial port: $SERIAL_PORT"
+  fi
+else
+  # Check if the specified serial port exists
+  if [ ! -e "$SERIAL_PORT" ]; then
+    log_and_echo "Specified serial port $SERIAL_PORT does not exist."
+    exit 1
+  else
+    log_and_echo "Specified serial port exists: $SERIAL_PORT"
+  fi
+fi
 
 ros2 run rosbot_xl_utils flash_firmware --port $SERIAL_PORT
 
