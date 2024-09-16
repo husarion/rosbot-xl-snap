c717282ab515f97bc61049d77869db05d01301a9
dominikn <dominik.nwk@gmail.com>
2024-09-16 20:12:35 +0000

    small fix

--- snap/local/restart_launcher.sh
+++ snap/local/restart_launcher.sh
@@ -3,7 +3,7 @@
 source $SNAP/usr/bin/utils.sh
 
 log "Restart ${SNAP_NAME}.daemon service"
-snapctl restart --enable ${SNAP_NAME}.daemon 2>&1 || true
+snapctl restart ${SNAP_NAME}.daemon 2>&1 || true
 
 log "Restart ${SNAP_NAME}.joy service"
-snapctl restart --enable ${SNAP_NAME}.joy 2>&1 || true
+snapctl restart ${SNAP_NAME}.joy 2>&1 || true
